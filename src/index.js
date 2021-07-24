44915dc1b35dc487a6d80f8a3e86262906372bc8
nstrong <nathanstrong.fx@gmail.com>
2021-07-23 17:12:41 -0700

    init

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ import '../assets/css/style.css';
 
 const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript HTML5 APIs</h1>';
+
+if ('draggable' in document.createElement('div')) {
+    init();
+}
+
+function init() {
+
+}
