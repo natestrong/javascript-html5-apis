384ec0ba2a078fc5e82312dbfa8eeb0f88822211
nstrong <nathanstrong.fx@gmail.com>
2021-07-23 17:13:12 -0700

    init

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import '../assets/css/style.css';
 const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript HTML5 APIs</h1>';
 
+// if div element has the draggable property, it is supported by the browser
 if ('draggable' in document.createElement('div')) {
     init();
 }
@@ -10,3 +11,5 @@ if ('draggable' in document.createElement('div')) {
 function init() {
 
 }
+
+
