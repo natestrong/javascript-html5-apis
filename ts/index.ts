f1765a4a26c1bfef530e850ba2740ee491786661
nstrong <nathanstrong.fx@gmail.com>
2021-07-26 21:04:38 -0700

    notifications

--- ts/index.ts
+++ ts/index.ts
@@ -1,5 +1,7 @@
 import '../assets/css/style.css';
 
+const ICON = 'https://cdn.dribbble.com/users/719060/screenshots/4785335/messi.jpg?compress=1&resize=400x300';
+
 const app = document.getElementById('app');
 app.innerHTML = `<h1>JavaScript HTML5 APIs</h1>`;
 
@@ -7,17 +9,13 @@ const init = async () => {
     const permission = await Notification.requestPermission();
 
     if (permission === 'granted') {
-
+        notify('hi frands', 'coolio');
     }
-
-    new Notification(
-        'Hi frand',
-        {
-            body:'Hello my dear good frand🦄.',
-            icon:'https://cdn.dribbble.com/users/719060/screenshots/4785335/messi.jpg?compress=1&resize=400x300',
-        })
-    ;
 };
+
+function notify(title:string, body:string) {
+    new Notification(title, {body, icon:ICON,});
+}
 
 if ('Notification' in window) {
     init();
