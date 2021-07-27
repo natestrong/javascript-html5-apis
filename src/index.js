4847f173792f80d99fcc3c9199f623c8879434c1
nstrong <nathanstrong.fx@gmail.com>
2021-07-26 20:39:39 -0700

    notifications

--- src/index.js
+++ src/index.js
@@ -3,17 +3,12 @@ const app = document.getElementById('app');
 app.innerHTML = `<h1>JavaScript HTML5 APIs</h1>`;
 const init = async () => {
     const permission = await Notification.requestPermission();
-    switch (permission) {
-        case 'default':
-            console.log('Neither granted nor denied');
-            break;
-        case 'denied':
-            console.log('Permission granted!');
-            break;
-        case 'granted':
-            alert('Coolio!');
-            break;
+    if (permission === 'granted') {
     }
+    new Notification('Hi frand', {
+        body: 'Hello my dear good frand🦄.',
+        icon: 'https://cdn.dribbble.com/users/719060/screenshots/4785335/messi.jpg?compress=1&resize=400x300',
+    });
 };
 if ('Notification' in window) {
     init();
