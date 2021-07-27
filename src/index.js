0ce2e2b2a2ea6f35c98abcfed938318351c3697c
nstrong <nathanstrong.fx@gmail.com>
2021-07-26 20:26:59 -0700

    notifications

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ const app = document.getElementById('app');
 app.innerHTML = `<h1>JavaScript HTML5 APIs</h1>`;
 const init = async () => {
     const permission = await Notification.requestPermission();
+    switch (permission) {
+        case 'default':
+            console.log('Neither granted nor denied');
+            break;
+        case 'denied':
+            console.log('Permission granted!');
+            break;
+        case 'granted':
+            alert('Coolio!');
+            break;
+    }
 };
 if ('Notification' in window) {
     init();
--- ts/index.ts
+++ ts/index.ts
@@ -5,6 +5,18 @@ app.innerHTML = `<h1>JavaScript HTML5 APIs</h1>`;
 
 const init = async () => {
     const permission = await Notification.requestPermission();
+
+    switch (permission) {
+        case 'default':
+            console.log('Neither granted nor denied');
+            break;
+        case 'denied':
+            console.log('Permission granted!');
+            break;
+        case 'granted':
+            alert('Coolio!');
+            break;
+    }
 };
 
 if ('Notification' in window) {
