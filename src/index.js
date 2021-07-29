e9b83f9111954ff35074cce0d9b513f1869e0449
nstrong <nathanstrong.fx@gmail.com>
2021-07-28 19:02:41 -0700

    notifications

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import '../assets/css/style.css';
 const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript HTML5 APIs</h1>';
-console.log(window.localStorage.constructor === Storage);
-console.log(window.sessionStorage.constructor === Storage);
-console.log(typeof Storage === 'function');
-console.log(!!(typeof Storage === 'function' && window.localStorage));
+console.log(window.localStorage);
