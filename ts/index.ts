31884b9c54a78a6aec49496da8b15339f4826ed4
nstrong <nathanstrong.fx@gmail.com>
2021-07-28 19:04:49 -0700

    notifications

--- ts/index.ts
+++ ts/index.ts
@@ -3,4 +3,5 @@ import '../assets/css/style.css';
 const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript HTML5 APIs</h1>';
 
-console.log(window.localStorage);
+const ls = window.localStorage;
+ls.setItem('cookies', 'true');
