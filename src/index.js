1cd829ed2d8e665b44858222691c5a45adc4a46d
nstrong <nathanstrong.fx@gmail.com>
2021-07-30 19:49:59 -0700

    notifications

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import '../assets/css/style.css';
+import './assets/css/style.css';
 const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript HTML5 APIs</h1>';
 const ls = window.localStorage;
-ls.setItem('cookies', 'true');
-console.log(ls.getItem('cookies') === 'true');
+const setValue = (key, value) => {
+    ls.setItem(key, JSON.stringify(value));
+};
+const getValue = (key) => {
+    return JSON.parse(ls.getItem(key));
+};
+setValue('cookies', true);
+const choice = getValue('cookies');
+console.log(choice);
