5944b8f62daab3ae8cbfde65bce2d157821c18c0
nstrong <nathanstrong.fx@gmail.com>
2021-08-04 06:06:36 -0700

    notifications

--- ts/index.ts
+++ ts/index.ts
@@ -1,46 +1,12 @@
 import './assets/css/style.css';
 
-import {storage} from './storage';
-
-const ls = storage('local');
-const ss = storage('session');
-
 const app = document.getElementById('app');
-app.innerHTML = `
-  <h1>JavaScript HTML5 APIs</h1>
-  <div style='display: none;' data-cookie>
-    <p>Would you like a cookie? 🍪</p>
-    <button type='button' data-cookie-accept>Yes</button>
-    <button type='button' data-cookie-reject>No</button>
-  </div>
-`;
+app.innerHTML = `<h1>JavaScript HTML5 APIs</h1>`;
 
-const init = () => {
-    const cookie = document.querySelector<HTMLElement>('[data-cookie]');
-    const accept = cookie.querySelector('[data-cookie-accept]');
-    const reject = cookie.querySelector('[data-cookie-reject]');
+console.log(window.navigator.geolocation);
 
-    const showCookie = () => (cookie.style.display = 'block');
-    const hideCookie = () => (cookie.style.display = 'none');
-
-    accept.addEventListener('click', () => {
-        hideCookie();
-        ls.set('cookies', true);
-    });
-
-    reject.addEventListener('click', () => {
-        hideCookie();
-        ss.set('cookies', false);
-    });
-
-    if (!cookie || ls.get('cookies') || ss.get('cookies') === false) {
-        return;
-    }
-
-    setTimeout(showCookie, 2000);
-};
-
-if (ls.isSupported && ss.isSupported) {
-    init();
+if ('geolocation' in navigator) {
+    console.log('Geolocation is supported!');
+} else {
+    console.log('Geolocation is not supported!');
 }
-
